ff993bfd36cc59cbd6c8cf0cc9ff33f599ebb3a8
likhil022 <likhilkosuru123@gmail.com>
2024-09-01 10:28:27 +0530

    hamburger menu

--- index.js
+++ index.js
@@ -25,3 +25,30 @@ function scroll(nav_button,section_id){
         section_id.scrollIntoView({ behavior: 'smooth' } );
     })
 }
+
+const navMain = document.querySelector(".nav_main");
+const navMin = document.querySelector(".nav_min");
+
+const navHam = document.querySelector(".nav_ham");
+const ham_status = false;
+const navList = document.querySelectorAll(".nav_li"); 
+const navClose = document.querySelector(".ham_close");
+const parentNav = document.querySelector('#nav');
+console.log(navClose);
+navHam.addEventListener('click',function(){
+    // navMain.classList.add('nav_hide');
+    navMin.classList.remove('nav_hide');
+    navList.forEach(function(nl){
+        console.log(nl);
+        nl.classList.add('nav_hide');
+    })
+
+})
+
+navClose.addEventListener('click',function(){
+    navMin.classList.add('nav_hide');
+    navList.forEach(function(nl){
+        console.log(nl);
+        nl.classList.remove('nav_hide');
+    })
+})
