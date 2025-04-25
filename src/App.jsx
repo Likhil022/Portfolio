2492b80a15311219fa288df73c9bf39e8ef1383a
likhil022 <likhilkosuru123@gmail.com>
2025-04-25 17:53:32 +0530

    profile picture

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
 import HeroSection from "./components/HeroSection";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    const cursorGlow = document.getElementById("cursor-glow");
+    const move = (e) => {
+      const x = e.clientX - cursorGlow.offsetWidth / 2;
+      const y = e.clientY - cursorGlow.offsetHeight / 2;
+      cursorGlow.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+    };
+    document.addEventListener("mousemove", move);
+    return () => document.removeEventListener("mousemove", move);
+  }, []);
   return (
-    <div className="px-24">
-      <HeroSection />
-    </div>
+    <>
+      <div
+        id="cursor-glow"
+        className="pointer-events-none fixed w-12 h-12 rounded-full bg-[#0870b8]
+                   shadow-[0_0_645px_rgba(8,112,184,1)] opacity-100 transition-all
+                   duration-75 blur-3xl z-50"
+      ></div>
+      <div className="px-36">
+        <HeroSection />
+      </div>
+    </>
   );
 }
 
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import "./herostyle.css";
+import AMB from "../assets/AMB.jpg";
 const HeroSection = () => {
   return (
     <>
-      <section className="relative h-screen bg-[radial-gradient(circle_at_top,_#1a1a2e,_#0f0f1e)]">
-        <div className="left">
-          <div className="flex flex-col gap-3 justify-center items-left h-screen w-[50%]">
+      <section className="relative h-screen flex basis-full justify-evenly">
+        <div className="left w-[70%] z-10">
+          <div className="flex flex-col gap-3 justify-center items-left h-screen">
             <p className="text-4xl font-semibold text-white">Hello, I'm</p>
             <h3 className="text-5xl font-semibold text-white">Likhil Kosuru</h3>
             <p className="text-2xl">Full Stack Developer</p>
@@ -18,6 +19,9 @@ const HeroSection = () => {
               <span class="green-dot"></span> Open to Freelance
             </div>
           </div>
+        </div>
+        <div className="right w-80 my-auto rounded-lg overflow-hidden z-20 mr-20 shadow-[0_0px_645px_rgba(8,_112,_184,_0.5)]">
+          <img src={AMB} alt="Hero.jpg" srcset="" className="" />
         </div>
       </section>
     </>
