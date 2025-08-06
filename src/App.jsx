42bcaa96fb882a32803a25f43aec0248a8c4e7b5
likhil022 <likhilkosuru123@gmail.com>
2025-08-06 09:06:29 +0530

    added scroll down and blur animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import HeroSection from "./components/HeroSection";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 import { useEffect } from "react";
 
 function App() {
@@ -12,17 +14,36 @@ function App() {
     document.addEventListener("mousemove", move);
     return () => document.removeEventListener("mousemove", move);
   }, []);
+
   return (
     <>
+      {/* Cursor Glow */}
       <div
         id="cursor-glow"
         className="pointer-events-none fixed w-12 h-12 rounded-full bg-[#0870b8]
                    shadow-[0_0_645px_rgba(8,112,184,1)] opacity-100 transition-all
                    duration-75 blur-3xl z-50"
       ></div>
-      <div className="px-36">
-        <HeroSection />
-      </div>
+
+      {/* Animate entire app */}
+      <motion.div
+        initial={{ opacity: 0, filter: "blur(10px)" }}
+        animate={{ opacity: 1, filter: "blur(0px)" }}
+        transition={{ duration: 1.2, ease: "easeOut" }}
+        className="text-white"
+      >
+        {/* Content wrapper */}
+        <section className="relative h-screen">
+          <div className="px-36 flex flex-col justify-center items-center">
+            <HeroSection />
+          </div>
+        </section>
+
+        {/* Future sections */}
+        {/* <About /> */}
+        {/* <Projects /> */}
+        {/* <Footer /> */}
+      </motion.div>
     </>
   );
 }
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import { ArrowDown } from "lucide-react";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
+
 import "./herostyle.css";
 import AMB from "../assets/AMB.jpg";
 const HeroSection = () => {
   return (
     <>
-      <section className="relative h-screen flex basis-full justify-evenly">
-        <div className="left w-[70%] z-10">
-          <div className="flex flex-col gap-3 justify-center items-left h-screen">
+      <section className="relative h-screen ">
+        <div className="flex basis-full justify-evenly gap-5">
+          <div className="left w-[70%] z-10 flex flex-col gap-3 justify-center items-left h-screen">
             <p className="text-4xl font-semibold text-white">Hello, I'm</p>
             <h3 className="text-5xl font-semibold text-white">Likhil Kosuru</h3>
             <p className="text-2xl">Full Stack Developer</p>
@@ -19,9 +22,23 @@ const HeroSection = () => {
               <span class="green-dot"></span> Open to Freelance
             </div>
           </div>
+
+          <div className="right w-80 my-auto rounded-lg overflow-hidden z-20 mr-20 shadow-[0_0px_645px_rgba(8,_112,_184,_0.5)]">
+            <img src={AMB} alt="Hero.jpg" srcset="" className="" />
+          </div>
         </div>
-        <div className="right w-80 my-auto rounded-lg overflow-hidden z-20 mr-20 shadow-[0_0px_645px_rgba(8,_112,_184,_0.5)]">
-          <img src={AMB} alt="Hero.jpg" srcset="" className="" />
+        <div className="absolute bottom-5 left-1/2 transform -translate-x-1/2 flex flex-col items-center text-white text-sm ">
+          <span className="mb-1 pb-1 opacity-70">Scroll Down</span>
+          <motion.div
+            animate={{ y: [0, 10, 0] }}
+            transition={{
+              duration: 1.2,
+              repeat: Infinity,
+              ease: "easeInOut",
+            }}
+          >
+            <ArrowDown className="opacity-60" />
+          </motion.div>
         </div>
       </section>
     </>
